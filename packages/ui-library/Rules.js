8b4e88bfde6ec7f4e1b5f6b503ba92700e9f3aaf
Ayyanababu Kopparthi <ayyanababu@gmail.com>
2025-04-06 16:41:50 +0530

    Update Rules.js

--- packages/ui-library/Rules.js
+++ packages/ui-library/Rules.js
@@ -1,1 +1,78 @@
 
+// should have
+// standard rules
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: ['./base.js'].map(require.resolve),
+  plugins: ['react', 'simple-import-sort', 'import'],
+  rules: {
+    'simple-import-sort/imports': [
+      'error',
+      {
+        groups: [['^react', '^@?\\w', '^(@arc|arc)', '^(src/)'], ['\n^[./]']]
+      }
+    ],
+    'simple-import-sort/exports': 'error',
+    'import/first': 'error',
+    'import/newline-after-import': 'error',
+    'import/no-duplicates': 'error',
+    'import/no-extraneous-dependencies': 0,
+    'react/jsx-no-useless-fragment': 'off',
+    'react/function-component-definition': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'react/no-unstable-nested-components': 'off',
+    'func-names': 0,
+    'no-console': 'off',
+    'react/prop-types': 'off'
+  }
+};
+
+
+
+
+// must have
+// base rules
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'eslint-config-airbnb',
+    'eslint-config-airbnb/hooks',
+    'eslint-config-airbnb-typescript',
+    'eslint-config-prettier'
+  ].map(require.resolve),
+  plugins: ['eslint-plugin-prettier'],
+  rules: {
+    'no-plusplus': 'off',
+    'react/require-default-props': 'off',
+    'max-lines': [
+      'warn',
+      {
+        max: 300,
+        skipBlankLines: true,
+        skipComments: true
+      }
+    ]
+  },
+  overrides: [
+    {
+      files: ['*.test.tsx', '*.cy.ts', '*.test.ts', '*.test.js'],
+      rules: {
+        'max-lines': 'off'
+      }
+    }
+  ]
+};
+
+
+// index.js
+module.exports = {
+  extends: ['./rules/standard'].map(require.resolve),
+  rules: {}
+};
+
