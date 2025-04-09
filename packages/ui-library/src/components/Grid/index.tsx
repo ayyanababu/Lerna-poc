b938dab38973e0d24764bc961a271c83bdeb32f5
Ayyanababu Kopparthi <ayyanababu@gmail.com>
2025-04-09 11:14:49 +0530

    Update index.tsx

--- packages/ui-library/src/components/Grid/index.tsx
+++ packages/ui-library/src/components/Grid/index.tsx
@@ -4,62 +4,62 @@ import useTheme from "../../hooks/useTheme";
 import { GridProps } from "./types";
 
 const Grid: React.FC<GridProps> = ({
-  width = 0,
-  height = 0,
-  xScale,
-  yScale,
-  numTicks = 5,
-  showHorizontal = true,
-  showVertical = false,
-  opacity = 0.3,
-  isVisible = false,
-  isLoading = false,
+    width = 0,
+    height = 0,
+    xScale,
+    yScale,
+    numTicks = 5,
+    showHorizontal = true,
+    showVertical = false,
+    opacity = 0.3,
+    isVisible = false,
+    isLoading = false,
 }) => {
-  const { theme } = useTheme();
+    const { theme } = useTheme();
 
-  if (!isVisible || isLoading) {
-    return null;
-  }
+    if (!isVisible || isLoading) {
+        return null;
+    }
 
-  return (
-    <g>
-      {/* Horizontal grid lines */}
-      {showHorizontal && yScale && (
-        <>
-          {yScale.ticks(numTicks).map((tick) => (
-            <line
-              key={`horizontal-${tick}`}
-              x1={0}
-              x2={width}
-              y1={yScale(tick)}
-              y2={yScale(tick)}
-              stroke={theme.colors.axis.grid}
-              strokeDasharray="2,2"
-              opacity={opacity}
-            />
-          ))}
-        </>
-      )}
+    return (
+        <g>
+            {/* Horizontal grid lines */}
+            {showHorizontal && yScale && (
+                <>
+                    {yScale.ticks(numTicks).map((tick) => (
+                        <line
+                            key={`horizontal-${tick}`}
+                            x1={0}
+                            x2={width}
+                            y1={yScale(tick)}
+                            y2={yScale(tick)}
+                            stroke={theme.colors.axis.grid}
+                            strokeDasharray="2,2"
+                            opacity={opacity}
+                        />
+                    ))}
+                </>
+            )}
 
-      {/* Vertical grid lines */}
-      {showVertical && xScale && (
-        <>
-          {xScale.ticks(numTicks).map((tick) => (
-            <line
-              key={`vertical-${tick}`}
-              x1={xScale(tick)}
-              x2={xScale(tick)}
-              y1={0}
-              y2={height}
-              stroke={theme.colors.axis.grid}
-              strokeDasharray="2,2"
-              opacity={opacity}
-            />
-          ))}
-        </>
-      )}
-    </g>
-  );
+            {/* Vertical grid lines */}
+            {showVertical && xScale && (
+                <>
+                    {xScale.ticks(numTicks).map((tick) => (
+                        <line
+                            key={`vertical-${tick}`}
+                            x1={xScale(tick)}
+                            x2={xScale(tick)}
+                            y1={0}
+                            y2={height}
+                            stroke={theme.colors.axis.grid}
+                            strokeDasharray="2,2"
+                            opacity={opacity}
+                        />
+                    ))}
+                </>
+            )}
+        </g>
+    );
 };
 
 export default Grid;
