9258b35019ec67427872711f736635ce82e08d13
Ayyanababu Kopparthi <ayyanababu@gmail.com>
2025-04-09 12:01:51 +0530

    Update types.ts

--- packages/ui-library/src/components/Tooltip/types.ts
+++ packages/ui-library/src/components/Tooltip/types.ts
@@ -1,14 +1,16 @@
 import { WithBoundingRectsProps } from "@visx/bounds";
 
+
 export interface TooltipData {
   label: string;
   value: number | string;
+  color?: string;
 }
 
 export interface TooltipProps extends WithBoundingRectsProps {
   top?: number;
   left?: number;
-  data?: TooltipData;
+  data?: TooltipData[];
   isVisible?: boolean;
-  containerRef?: React.RefObject<HTMLElement>;
+  containerRef?: React.RefObject<HTMLElement>; 
 }
