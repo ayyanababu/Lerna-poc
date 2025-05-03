daeb73f88220518c2359236989ee68bdc401a62f
Wajid2001 <67428865+Wajid2001@users.noreply.github.com>
2025-05-03 06:19:38 -0600

    add no data thing in chart wrapper

--- packages/ui-library/src/components/ChartWrapper/index.tsx
+++ packages/ui-library/src/components/ChartWrapper/index.tsx
@@ -31,9 +31,11 @@ export const ChartWrapper = forwardRef<HTMLDivElement, ChartWrapperProps>(
     },
     ref,
   ) => {
-    const { theme } = useTheme()
+    const { theme } = useTheme();
     const containerRef = useRef<HTMLDivElement>(null);
     const [canRender, setCanRender] = React.useState(true);
+
+    const isHorizontal = legendsProps?.position === LegendPosition.LEFT || legendsProps?.position === LegendPosition.RIGHT;
 
     const {
       colorScale = defaultColorScale,
@@ -74,13 +76,13 @@ export const ChartWrapper = forwardRef<HTMLDivElement, ChartWrapperProps>(
           gap: "0px", // Removed parent gap to control internal gaps more precisely
           flex: 1,
         }}
-        ref={containerRef}
+        // ref={containerRef}
       >
         {canRender ? (
           <>
             {/* Only render title if it exists */}
             {!isLoading && title && <Title title={title} {...titleProps} />}
-            
+
             <Box
               sx={{
                 position: "relative",
@@ -91,8 +93,7 @@ export const ChartWrapper = forwardRef<HTMLDivElement, ChartWrapperProps>(
                 // commented by VNS
                 gap: position === LegendPosition.BOTTOM ? "12px" : "20px",
                 marginTop: title ? "12px" : "0px", // Add 12px gap only if title exists
-                ...(position === LegendPosition.LEFT ||
-                position === LegendPosition.RIGHT
+                ...(isHorizontal
                   ? {
                       flexDirection:
                         position === LegendPosition.LEFT
@@ -116,13 +117,20 @@ export const ChartWrapper = forwardRef<HTMLDivElement, ChartWrapperProps>(
                 />
               )}
               <Box
-                ref={ref}
+                ref={(el) => {
+                  if (containerRef) containerRef.current = el;
+                  if (typeof ref === 'function') {
+                    ref(el);
+                  } else if (ref) {
+                    ref.current = el;
+                  }
+                }}
                 sx={{
                   position: "relative",
-                  height: "100%",
-                  width: "100%",
+                  height:  "100%",
+                  width: isHorizontal ?  "50%" : "100%",
                   display: "flex",
-                  flex: "1 1 100%",
+                  flex: isHorizontal ?  "1 1 50%": "1 1 100%",
                   minHeight: 0,
                 }}
               >
@@ -140,10 +148,58 @@ export const ChartWrapper = forwardRef<HTMLDivElement, ChartWrapperProps>(
                   >
                     <DotLoader />
                   </Box>
-                ) : legendData.length > 0 ? (
+                ) : (legendData.length > 0 && children) ? (
                   children
                 ) : (
-                  <div>No data to display.</div>
+                  <Box
+                    sx={{
+                      display: "flex",
+                      flexDirection: "column",
+                      justifyContent: "center",
+                      textAlign: "center",
+                      gap: "12px",
+                      alignItems: "center",
+                      height: "100%",
+                      width: "100%",
+                      backgroundColor: `${theme.colors.common.text}08`,
+                      borderRadius: "8px",
+                      margin: "auto",
+                      padding: "16px",
+                      boxSizing: "border-box",
+                    }}
+                  >
+                    <svg
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        opacity="0.2"
+                        d="M20.25 7.5C20.25 9.98531 16.5562 12 12 12C7.44375 12 3.75 9.98531 3.75 7.5C3.75 5.01469 7.44375 3 12 3C16.5562 3 20.25 5.01469 20.25 7.5Z"
+                        fill={theme.colors.common.text}
+                      />
+                      <path
+                        d="M12 2.25C6.95344 2.25 3 4.55625 3 7.5V16.5C3 19.4438 6.95344 21.75 12 21.75C17.0466 21.75 21 19.4438 21 16.5V7.5C21 4.55625 17.0466 2.25 12 2.25ZM19.5 12C19.5 12.9019 18.7612 13.8216 17.4741 14.5238C16.0247 15.3141 14.0803 15.75 12 15.75C9.91969 15.75 7.97531 15.3141 6.52594 14.5238C5.23875 13.8216 4.5 12.9019 4.5 12V10.44C6.09938 11.8463 8.83406 12.75 12 12.75C15.1659 12.75 17.9006 11.8425 19.5 10.44V12ZM6.52594 4.97625C7.97531 4.18594 9.91969 3.75 12 3.75C14.0803 3.75 16.0247 4.18594 17.4741 4.97625C18.7612 5.67844 19.5 6.59812 19.5 7.5C19.5 8.40188 18.7612 9.32156 17.4741 10.0237C16.0247 10.8141 14.0803 11.25 12 11.25C9.91969 11.25 7.97531 10.8141 6.52594 10.0237C5.23875 9.32156 4.5 8.40188 4.5 7.5C4.5 6.59812 5.23875 5.67844 6.52594 4.97625ZM17.4741 19.0238C16.0247 19.8141 14.0803 20.25 12 20.25C9.91969 20.25 7.97531 19.8141 6.52594 19.0238C5.23875 18.3216 4.5 17.4019 4.5 16.5V14.94C6.09938 16.3463 8.83406 17.25 12 17.25C15.1659 17.25 17.9006 16.3425 19.5 14.94V16.5C19.5 17.4019 18.7612 18.3216 17.4741 19.0238Z"
+                        fill={theme.colors.common.text}
+                      />
+                    </svg>
+
+                    <span
+                      style={{
+                        color: theme.colors.common.text,
+                        fontFamily: 'Roboto',
+                        fontStyle: 'normal', 
+                        fontWeight: 400,
+                        fontSize: '12px',
+                        lineHeight: '143%',
+                        letterSpacing: '0.4px',
+                      }}
+                    >
+                      There are no {title} data available
+                    </span>
+                  </Box>
                 )}
               </Box>
             </Box>
